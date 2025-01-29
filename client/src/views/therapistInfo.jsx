e63a28a13c7a7866966a2d8da8053393e0a84b96
Jonathan Fajobi <ralufajobi@outlook.com>
2025-01-29 22:39:31 +0000

    layouts created, some boilerplate text

--- client/src/views/therapistInfo.jsx
+++ client/src/views/therapistInfo.jsx
@@ -4,55 +4,72 @@ import { animationOptions } from '../styles/animations';
 const therapistInfo = () => {
   return (
     <div className="container" style={{ margin: '15vh 5vw' }}>
-      <h1 className="display-3 fw-bolder mb-5" id="dashboard-title">
-        Additional Information | Therapists
-      </h1>
-      <section style={{ margin: '30vh 0vw' }}>
-        <h2>A good therapist</h2>
-        <p></p>
+      <section style={{ margin: '0vh 0vw 30vh 0vw' }}>
+        <h1 className="display-3 fw-bolder mb-5" id="dashboard-title">
+          Additional Information | Therapists
+        </h1>
+        <p className="text-center display-6"></p>
       </section>
 
       <section style={{ margin: '30vh 0vw' }}>
-        <div className="row justify-content-between">
+        <div className="row justify-content-evenly">
           <motion.div
             {...animationOptions}
             className="col-md-5 chiryo_rounded chiryo_primary p-3 p-md-5 h-full d-flex flex-column justify-content-between"
           >
             <h1 className="text-center display-4 fw-bolder mb-3">
-              Find a Therapist
+              Technology and Reliability
             </h1>
             <p className="text-center display-6">
               Get access to people who can provide the help you need with no
               hassle
             </p>
-            <div className="d-flex justify-content-center mt-auto">
-              <button className="display-6 fw-bolder text-dark chiryo_shadow chiryo_rounded chiryo_primary_action">
-                <a
-                  href="/questionnaire/"
-                  style={{ textDecoration: 'none', color: 'inherit' }}
-                >
-                  Try it!
-                </a>
-              </button>
-            </div>
           </motion.div>
 
           <div className="col-md-5 chiryo_rounded chiryo_primary p-3 p-md-5 h-full d-flex flex-column justify-content-between">
             <h1 className="text-center display-4 fw-bolder mb-3">
-              more yap to see if the styling fits
+              Client Expectations
             </h1>
             <p className="text-center display-6">yap yap yap</p>
           </div>
         </div>
       </section>
 
-      <section className="" style={{ margin: '30vh 0vw' }}>
+      <section style={{ margin: '30vh 0vw' }}>
+        <div className="mb-3 mb-md-0 chiryo_rounded chiryo_primary p-3 p-md-5">
+          <h1 className="text-center display-4 fw-bolder mb-3">
+            Client Expectations
+          </h1>
+          <p className="text-center display-6">yap yap yap</p>
+        </div>
+      </section>
+
+      <section style={{ margin: '30vh 0vw' }}>
         <h2 className="display-3 fw-bolder mb-5">
-          Why join us instead of other sites?
+          Why join Chiryo instead of other sites?
         </h2>
-        <p className="chiryo_rounded chiryo_primary display-6">
-          Something about this service having more flexibility than places
-        </p>
+        <div className="mb-3 mb-md-0 chiryo_rounded chiryo_primary p-3 p-md-5">
+          <ul>
+            <li>
+              <b>Set Your Own Schedule</b> – Take on as many (or as few) clients
+              as you like. Your time, your rules.
+            </li>
+            <li>
+              <b>Secure & Seamless</b> – Our platform is designed with privacy
+              and security in mind, so both you and your clients can feel at
+              ease.
+            </li>
+            <li>
+              <b>Expand Your Reach</b> – Connect with clients beyond your local
+              area and provide accessible care to those who need it.
+            </li>
+          </ul>
+          <h2>
+            Signing up is simple, and we’re here to support you every step of
+            the way. Join a community of professionals making therapy more
+            accessible, one session at a time!
+          </h2>
+        </div>
       </section>
     </div>
   );
--- client/src/views/userInfo.jsx
+++ client/src/views/userInfo.jsx
@@ -3,10 +3,65 @@ import { animationOptions } from '../styles/animations';
 
 const userInfo = () => {
   return (
-    <div
-      className="container-fluid main-container"
-      style={{ margin: '30vh 0vw' }}
-    ></div>
+    <div className="container" style={{ margin: '15vh 5vw' }}>
+      <section style={{ margin: '0vh 0vw 30vh 0vw' }}>
+        <h1
+          className="display-3 text-center fw-bolder mb-5"
+          id="dashboard-title"
+        >
+          Why is mental health important?
+        </h1>
+        <p className="text-center display-6">
+          This is a test to see what it will look like on the page
+        </p>
+      </section>
+      <section style={{ margin: '30vh 0vw' }}>
+        <h1
+          className="display-3 text-center fw-bolder mb-5"
+          id="dashboard-title"
+        >
+          Ways of improving mental health
+        </h1>
+        <div className="row justify-content-evenly">
+          <motion.div
+            {...animationOptions}
+            className="col-md-5 chiryo_rounded chiryo_primary p-3 p-md-5 h-full d-flex flex-column justify-content-between"
+          >
+            <h1 className="text-center display-4 fw-bolder mb-3">
+              Find a Therapist
+            </h1>
+            <p className="text-center display-6">
+              Get access to people who can provide the help you need with no
+              hassle
+            </p>
+          </motion.div>
+
+          <div className="col-md-5 chiryo_rounded chiryo_primary p-3 p-md-5 h-full d-flex flex-column justify-content-between">
+            <h1 className="text-center display-4 fw-bolder mb-3">
+              more yap to see if the styling fits
+            </h1>
+            <p className="text-center display-6">yap yap yap</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="" style={{ margin: '30vh 0vw' }}>
+        <h2 className="display-3 fw-bolder mb-5">Other Recommended Services</h2>
+        <div className="mb-3 mb-md-0 chiryo_rounded chiryo_primary p-3 p-md-5">
+          <h2>You must have...</h2>
+          <ul>
+            <li>Testimonials from previous clients</li>
+            <li>
+              Degree in field of Psychology, Nursing or other Mental
+              Health/Social Work Studies
+            </li>
+            <li>Strong & demonstrable communication skills</li>
+            <li>Professional Accreditation (BACP, NCS, UKCP)</li>
+            <li>Good Vibes ☀️</li>
+          </ul>
+        </div>
+      </section>
+    </div>
   );
 };
 
