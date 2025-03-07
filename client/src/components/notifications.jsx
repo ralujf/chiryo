8897ff20cadc326021756d0d474684ae94fb899f
Jonathan Fajobi <ralufajobi@outlook.com>
2025-03-07 19:17:56 +0000

    notifications

---
+++ client/src/components/notifications.jsx
@@ -0,0 +1,7 @@
+import { ToastContainer, toast } from 'react-toastify';
+
+const NotificationContainer = <></>;
+const SuccessNotification = <></>;
+const ErrorNotification = <></>;
+
+export { SuccessNotification, ErrorNotification, NotificationContainer };
