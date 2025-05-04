b454d674fe78e3c4ec6d5e5751e850a6257b3d5e
Jonathan Fajobi <ralufajobi@outlook.com>
2025-05-04 23:20:35 +0100

    frontend api changes, ui tooltips, assets, minor changes to interactions

---
+++ server/localSetup.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
+const Therapist = require('./models/therapist');
+const Application = require('./models/application');
+const { startServer, closeServer } = require('./index');
+
+const therapistData = require('./__fixtures__/therapists.data.json');
+const applicantData = require('./__fixtures__/applicants.data.json');
+
+const runSetup = async () => {
+  try {
+    await startServer();
+
+    await Therapist.deleteMany({});
+    await Application.deleteMany({});
+    await User.deleteMany({});
+
+    for (const therapistJSON of therapistData) {
+      const item = new Therapist(therapistJSON);
+      await item.save();
+    }
+
+    for (const applicantJSON of applicantData) {
+      const item = new Application(applicantJSON);
+      await item.save();
+    }
+
+    const SALT_ROUNDS = 13;
+
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash('undergrad', salt);
+
+    const adminUser = {
+      adminId: process.env.ADMIN,
+      username: 'admin',
+      email: 'r.fajobi@qmul.ac.uk',
+      password: hashedPassword,
+      age: '22',
+      race: 'N/A',
+      religion: 'N/A',
+      location: 'N/A',
+      additional: 'N/A',
+      problem: 'N/A',
+      firstLogin: false,
+    };
+
+    const admin = new User(adminUser);
+    await admin.save();
+  } catch (err) {
+    console.error('Error during setup: ', err);
+  } finally {
+    await closeServer();
+    console.log('\x1b[33m%s\x1b[0m', 'STATUS: SETUP COMPLETE', '\x1b[0m');
+  }
+};
+
+runSetup();
